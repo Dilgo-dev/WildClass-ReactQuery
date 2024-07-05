1e5ff90a8cdc9cf20df77585a2ac1d3f224e34b0
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-07-05 14:20:44 +0200

    ✨ Add feature choose pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchPokemon from "./services/fetchPokemons";
+import { useState } from "react";
+
 export default function App() {
-    return <></>;
+    const [id, setId] = useState(1);
+    const { data, isLoading, isError, refetch } = useQuery({
+        queryKey: ["pokemon", id],
+        queryFn: () => fetchPokemon(id),
+    });
+
+    const handleChangeId = (event) => {
+        setId(event.target.value);
+        refetch();
+    };
+
+    return (
+        <>
+            {isError ? (
+                <p>🚏 Une erreur est survenue lors du chargement du pokemon</p>
+            ) : null}
+            {isLoading ? <p>🐲 Chargement du pokemon ! ...</p> : null}
+            {data ? (
+                <>
+                    <img src={data.sprites.front_default} alt={data.name} />
+                    <input type="number" onChange={handleChangeId} value={id} />
+                </>
+            ) : null}
+        </>
+    );
 }
