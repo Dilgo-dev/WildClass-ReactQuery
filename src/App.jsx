172ce2df423d0abb279286234f373fdfbac91248
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-07-05 14:56:42 +0200

    🥅 Handling response error

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchPokemon from "./services/fetchPokemons";
+
 export default function App() {
-    return <></>;
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ["pokemon"],
+        queryFn: fetchPokemon,
+        retry: false,
+    });
+
+    return (
+        <>
+            {isError ? (
+                <p>
+                    🚏 Une erreur est survenue lors du chargement du pokemon:{" "}
+                    {error.message}
+                </p>
+            ) : null}
+            {isLoading ? <p>🐲 Chargement du pokemon ! ...</p> : null}
+            {data ? (
+                <>
+                    <img src={data.sprites.front_default} alt={data.name} />
+                </>
+            ) : null}
+        </>
+    );
 }
--- src/services/fetchPokemons.js
+++ src/services/fetchPokemons.js
@@ -2,8 +2,13 @@ import axios from "axios";
 
 const fetchPokemon = () => {
     return axios
-        .get("https://pokeapi.co/api/v2/pokemon?limit=10")
-        .then((response) => response.data);
+        .get("https://pokeapi.co/api/v2/pokemo")
+        .then((response) => response.data)
+        .catch(() => {
+            throw new Error(
+                "Impossible de récupérer les données du Pokemon. 🧯"
+            );
+        });
 };
 
 export default fetchPokemon;
