a64f94486297f1106485c4507b4ac03ba9a6657a
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-07-05 15:02:26 +0200

    ✨ Add random pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchRandomPokemon from "./services/fetchRandomPokemons";
+
 export default function App() {
-    return <></>;
+    const { data, isLoading, isError, refetch } = useQuery({
+        queryKey: ["pokemon"],
+        queryFn: fetchRandomPokemon,
+    });
+
+    return (
+        <>
+            {isError ? (
+                <p>🚏 Une erreur est survenue lors du chargement du pokemon</p>
+            ) : null}
+            {isLoading ? <p>🐲 Chargement du pokemon ! ...</p> : null}
+            {data ? (
+                <>
+                    <img src={data.sprites.front_default} alt={data.name} />
+                    <button onClick={refetch}>Recharger</button>
+                </>
+            ) : null}
+        </>
+    );
 }
